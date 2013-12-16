49e5aa1e7309d8f2aa862456a30206a8174e0b78
ElevatedEdge <=>
2013-12-15 17:53:34 -0800

    Changed the game base
    +New textures

--- EdgeLibraryMain/EdgeLibrary-Test/EdgeLibrary-Test/MainGame.cs
+++ EdgeLibraryMain/EdgeLibrary-Test/EdgeLibrary-Test/MainGame.cs
@@ -18,6 +18,7 @@ namespace EdgeLibrary_Test
     /// TODO:
     /// -General
     ///     -Fix "Remove Element/Object" function
+    ///     -Add basic collision functions
     /// -Actions
     ///     -Fix "EActionSequence"
     ///     -Fix "EActionRotate"?
@@ -30,7 +31,7 @@ namespace EdgeLibrary_Test
     /// <summary>
     /// MUSIC AND TEXTURES:
     /// - cynicmusic.com/pixelsphere.org
-    /// - IMakeGames at OpenGameArt.org
+    /// - MoikMellah at OpenGameArt.org
     /// </summary>
     /// 
 
@@ -91,8 +92,9 @@ namespace EdgeLibrary_Test
             edgeGame.LoadTexture("Particle Textures/smoke", "smoke");
             edgeGame.LoadTexture("Particle Textures/snow", "snow");
 
-            edgeGame.LoadTexture("Platform/Entities/sprite01", "sprite01");
-            edgeGame.LoadTexture("Platform/Entities/sprite02", "sprite02");
+            edgeGame.LoadTexture("Players/Ninja/ninja1", "ninja");
+            edgeGame.LoadTexture("Players/Normal/player1", "player");
+            edgeGame.LoadTexture("Statues/sprite1", "statues");
         }
 
         private void initializeGameWindow()
@@ -107,6 +109,41 @@ namespace EdgeLibrary_Test
         {
             EScene menuScene = new EScene("menuScene");
             edgeGame.addScene(menuScene);
+
+            ESprite sprite = new ESprite("player", new Vector2(450, 450), 50, 100);
+            menuScene.addElement(sprite);
+
+            #region PARTICLES
+            EParticleEmitter dotsEmitter = new EParticleEmitter("snow", new Vector2(500, 0));
+            dotsEmitter.ShouldEmit = true;
+            dotsEmitter.DrawLayer = 1;
+            dotsEmitter.EmitPositionVariance = new ERangeArray(ERange.RangeWithDiffer(0, 900), ERange.RangeWithDiffer(0, 0));
+            dotsEmitter.ColorVariance = new ERangeArray(new ERange(100), new ERange(100), new ERange(100), new ERange(255));
+            dotsEmitter.VelocityVariance = new ERangeArray(ERange.RangeWithDiffer(0, 0.1f), ERange.RangeWithDiffer(8, 2.5f));
+            dotsEmitter.SizeVariance = new ERangeArray(new ERange(15), new ERange(15));
+            dotsEmitter.GrowSpeed = 0f;
+            dotsEmitter.StartRotationVariance = ERange.RangeWithDiffer(0, 0);
+            dotsEmitter.RotationSpeedVariance = ERange.RangeWithDiffer(0, 0);
+            dotsEmitter.LifeVariance = new ERange(5000);
+            dotsEmitter.EmitWait = 0;
+            menuScene.addElement(dotsEmitter);
+
+            EParticleEmitter mouseEmitter = new EParticleEmitter("fire", new Vector2(400, 400));
+            mouseEmitter.ShouldEmit = true;
+            mouseEmitter.DrawLayer = 3;
+            mouseEmitter.EmitPositionVariance = new ERangeArray(new ERange(0), new ERange(0));
+            mouseEmitter.ColorVariance = new ERangeArray(new ERange(0), new ERange(40, 80), new ERange(40, 80), new ERange(255));
+            mouseEmitter.VelocityVariance = new ERangeArray(ERange.RangeWithDiffer(0, 4), ERange.RangeWithDiffer(0, 4));
+            mouseEmitter.SizeVariance = new ERangeArray(ERange.RangeWithDiffer(100, 25), ERange.RangeWithDiffer(100, 25));
+            mouseEmitter.GrowSpeed = 1f;
+            mouseEmitter.StartRotationVariance = ERange.RangeWithDiffer(0, 0);
+            mouseEmitter.RotationSpeedVariance = ERange.RangeWithDiffer(0, 0);
+            mouseEmitter.LifeVariance = new ERange(1000);
+            mouseEmitter.EmitWait = 0;
+            mouseEmitter.ActionToRunOnParticles = new EActionFollow(sprite, 4);
+            mouseEmitter.ClampToMouse();
+            menuScene.addElement(mouseEmitter);
+            #endregion
         }
 
         private void initializeGameScene()
