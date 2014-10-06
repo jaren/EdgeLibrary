cd5f9fc15797d59a14b3ba27ed53102a6b08eef0
Fifty Two <=>
2014-10-05 17:20:13 -0700

    :octocat:

--- EdgeLibraryMain/EdgeDemo/EdgeDemo/CheckersGame/BoardManager.cs
+++ EdgeLibraryMain/EdgeDemo/EdgeDemo/CheckersGame/BoardManager.cs
@@ -10,7 +10,7 @@ namespace EdgeDemo.CheckersGame
     public class BoardManager : Sprite
     {
         public Board Board;
-        public Sprite StatusSprite;
+        public TextSprite StatusSprite;
 
         //Used for each move
         public bool TopTeamTurn;
@@ -26,7 +26,7 @@ namespace EdgeDemo.CheckersGame
         public BoardManager()
             : base("", Vector2.Zero)
         {
-            Board = new Board("Pixel", EdgeGame.WindowSize/2, 8, 64, 0, Color.SaddleBrown, Color.Tan, 5, Color.DarkGoldenrod, "Checkers", 50, TopColor, BottomColor);
+            Board = new Board("Pixel", EdgeGame.WindowSize/2, 8, 64, 0, Color.SaddleBrown, Color.Tan, 5, Color.DarkGoldenrod, "Checkers", 54, TopColor, BottomColor);
             Board.AddToGame();
 
             DebugText debug = new DebugText("Impact-20", new Vector2(0, EdgeGame.WindowSize.Y - 75)) { Color = Color.Goldenrod, CenterAsOrigin = false };
@@ -34,7 +34,7 @@ namespace EdgeDemo.CheckersGame
 
             StatusSprite = new TextSprite("ComicSans-20", "Top Team: Select a square to move from", Vector2.Zero);
             StatusSprite.CenterAsOrigin = false;
-            //StatusSprite.AddToGame();
+            StatusSprite.AddToGame();
         }
 
         public string Move(int startX, int startY, int finishX, int finishY)
@@ -77,7 +77,7 @@ namespace EdgeDemo.CheckersGame
                 return "Please click on a square which is on the board";
             }
 
-            return "";
+            return "☺";
 
             //Eventually this will call the web service's move function
             //Something like this:
@@ -101,13 +101,16 @@ namespace EdgeDemo.CheckersGame
                     {
                         StartX = square.X;
                         StartY = square.Y;
-                        square.Color = Color.Green;
+                        square.Color = Color.DarkGreen;
+                        SelectedFirstSquare = true;
                     }
                     else
                     {
                         FinishX = square.X;
                         FinishY = square.Y;
-                        square.Color = Color.Red;
+                        square.Color = Color.DarkRed;
+                        StatusSprite.Text = Move(StartX, StartY, FinishX, FinishY);
+                        SelectedFirstSquare = false;
                     }
                 }
             }
