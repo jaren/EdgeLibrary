72d1ff5490b3a38cd06512d133c8cf228fbcdf43
Aaron Opell <aaron.opell@live.com>
2014-11-30 16:29:43 -0800

    WCFSERVICE:one:

---
+++ EdgeLibraryMain/WcfService1/WcfService1/CheckersService.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace CheckersService
+{
+    public class CheckersService : ICheckersService
+    {
+        public short[] movePiece(short pieceId, short destX, short destY)
+        {
+            return new short[] { pieceId, destX, destY };
+        }
+    }
+}
---
+++ EdgeLibraryMain/WcfService1/WcfService1/ICheckersService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace CheckersService
+{
+    [ServiceContract]
+    public interface ICheckersService
+    {
+        [OperationContract]
+        short[] movePiece(short pieceID, short destX, short destY);
+    }
+}
