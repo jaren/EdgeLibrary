85160525287390fc2b7ec35a2deaf62c8a07e956
Aaron Opell <aaron.opell@live.com>
2015-05-03 17:57:40 -0700

    Added Base Classes to Tower Defense :flags:

---
+++ EdgeLibraryMain/TowerDefenseGame/TowerDefenseGame/Level/Level.cs
@@ -0,0 +1,19 @@
+﻿using EdgeLibrary;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TowerDefenseGame
+{
+    public class Level : Sprite
+    {
+        public List<Vector2> Waypoints;
+
+        public Level(List<Vector2> waypoints, string texture) : base(texture, Vector2.Zero)
+        {
+            Waypoints = waypoints;
+        }
+    }
+}
---
+++ EdgeLibraryMain/TowerDefenseGame/TowerDefenseGame/Level/Round.cs
@@ -0,0 +1,27 @@
+﻿using EdgeLibrary;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TowerDefenseGame
+{
+    public class Round : Ticker
+    {
+        public Dictionary<EnemyType, float> Enemies;
+        public int CurrentIndex = 0;
+
+        public Round(Dictionary<EnemyType, float> enemies) : base(0)
+        {
+            Enemies = enemies;
+            base.OnTick += Round_OnTick;
+        }
+
+        void Round_OnTick(GameTime gameTime)
+        {
+            CurrentIndex++;
+            MillisecondsWait = Enemies.Values.ToList()[CurrentIndex];
+        }
+    }
+}
